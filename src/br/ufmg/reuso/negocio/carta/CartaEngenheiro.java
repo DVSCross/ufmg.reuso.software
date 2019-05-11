466c993e67415f446fd43df75c00569b85ee31f0
danielvsc <danielvsc@gmail.com>
2019-05-11 14:59:41 -0300

    Refatoração Extract Class para remoção de Bad Smell God Class na classe CartaEngenheiro.

--- src/br/ufmg/reuso/negocio/carta/CartaEngenheiro.java
+++ src/br/ufmg/reuso/negocio/carta/CartaEngenheiro.java
@@ -18,11 +18,9 @@ import java.io.IOException;
  */
 
 public class CartaEngenheiro extends Carta
-{
-	private String nomeEngenheiro;								//variável que guarda nome do engenheiro
-	private int salarioEngenheiro;								//variável que guarda salário do engenheiro
-	private int habilidadeEngenheiro;							//variável que guarda habilidade do engenheiro
-	private int maturidadeEngenheiro;							//variável que guarda a maturidade do engenheiro
+{	
+	private Engenheiro engenheiro;
+	
 	private boolean engenheiroTrabalhouNestaRodada; 			/**se true-> trabalhou, se false-> não trabalhou na rodada*/ 
 	
 	private int habilidadeEngenheiroAtual;						/**contém a pontos de habilidade que engenheiro tem na rodada*/ 
@@ -32,12 +30,12 @@ public class CartaEngenheiro extends Carta
 	{
 		super ("Eng.Software", codigo, texto,ENGENHEIRO);				 //inicializando a superclasse explicitamente
 		
-		setNomeEngenheiro(nomeEng);
-		setSalarioEngenheiro(salarioEng);
-		setHabilidadeEngenheiro(habilidadeEng);
-		setHabilidadeEngenheiroAtual(getHabilidadeEngenheiro());		/**habilidade atual na construção da carta do engenheiro é igual à habildade da carta*/
-		setMaturidadeEngenheiro(maturidadeEng);
+		this.engenheiro.setNomeEngenheiro(nomeEng);
+		this.engenheiro.setSalarioEngenheiro(salarioEng);
+		this.engenheiro.setHabilidadeEngenheiro(habilidadeEng);		
+		this.engenheiro.setMaturidadeEngenheiro(maturidadeEng);
 		this.engenheiroTrabalhouNestaRodada=false;
+		this.setHabilidadeEngenheiroAtual(this.engenheiro.getHabilidadeEngenheiro()); /**habilidade atual na construção da carta do engenheiro é igual à habildade da carta*/
 	}
 	
 		
@@ -47,46 +45,6 @@ public class CartaEngenheiro extends Carta
     // printa a carta
 	}
 	
-	public String getNomeEngenheiro() 
-	{
-		return nomeEngenheiro;
-	}
-
-	public void setNomeEngenheiro(String nomeEngenheiro) 
-	{
-		this.nomeEngenheiro = nomeEngenheiro;
-	}
-
-	public int getSalarioEngenheiro() 
-	{
-		return salarioEngenheiro;
-	}
-
-	public void setSalarioEngenheiro(int salarioEngenheiro) 
-	{
-		this.salarioEngenheiro = salarioEngenheiro;
-	}
-
-	public int getHabilidadeEngenheiro() 
-	{
-		return habilidadeEngenheiro;
-	}
-
-	public void setHabilidadeEngenheiro(int habilidadeEngenheiro) 
-	{
-		this.habilidadeEngenheiro = habilidadeEngenheiro;
-	}
-
-	public int getMaturidadeEngenheiro() 
-	{
-		return maturidadeEngenheiro;
-	}
-
-	public void setMaturidadeEngenheiro(int maturidadeEngenheiro) 
-	{
-		this.maturidadeEngenheiro = maturidadeEngenheiro;
-	}
-
 	public boolean isEngenheiroTrabalhouNestaRodada() 
 	{
 		return engenheiroTrabalhouNestaRodada;
@@ -112,10 +70,10 @@ public class CartaEngenheiro extends Carta
 			FileWriter writer = new FileWriter(path, true);
 			writer.write("codigo = " + this.codigoCarta + "\n");
 			writer.write("texto = " + this.textoCarta + "\n");
-			writer.write("nome = " + this.nomeEngenheiro + "\n");
-			writer.write("salario = " + this.salarioEngenheiro + "\n");
-			writer.write("habilidade = " + this.habilidadeEngenheiro + "\n");
-			writer.write("maturidade = " + this.maturidadeEngenheiro + "\n");
+			writer.write("nome = " + this.engenheiro.getNomeEngenheiro() + "\n");
+			writer.write("salario = " + this.engenheiro.getSalarioEngenheiro() + "\n");
+			writer.write("habilidade = " + this.engenheiro.getHabilidadeEngenheiro() + "\n");
+			writer.write("maturidade = " + this.engenheiro.getMaturidadeEngenheiro() + "\n");
 			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block			
